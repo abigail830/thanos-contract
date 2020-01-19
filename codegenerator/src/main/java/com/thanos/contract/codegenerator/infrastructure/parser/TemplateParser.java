0895e628cb5c18fd1d340afa1e0e3610554cf4f8
SaraQian <abigail830@163.com>
2020-01-19 14:54:44 +0800

    update parser to file

--- codegenerator/src/main/java/com/thanos/contract/codegenerator/infrastructure/parser/TemplateParser.java
+++ codegenerator/src/main/java/com/thanos/contract/codegenerator/infrastructure/parser/TemplateParser.java
@@ -4,7 +4,9 @@ import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Map;
 
@@ -16,10 +18,17 @@ public class TemplateParser {
         mf = new DefaultMustacheFactory();
     }
 
-    public String parseJunitTemplate(Map<String, Object> content) throws IOException {
+    public String parseJunitTemplateToString(Map<String, Object> content) throws IOException {
         Mustache mustache = mf.compile("template/junit.template");
         final StringWriter writer = new StringWriter();
         mustache.execute(writer, content).flush();
         return writer.toString();
+    }
+
+    public void parseJunitTemplateToFile(Map<String, Object> content, String outputFile) throws IOException {
+        Mustache mustache = mf.compile("template/junit.template");
+        final PrintWriter writer = new PrintWriter(new FileOutputStream(outputFile), true);
+        mustache.execute(writer, content).flush();
+
     }
 }
--- codegenerator/src/test/java/com/thanos/contract/codegenerator/infrastructure/parser/TemplateParserTest.java
+++ codegenerator/src/test/java/com/thanos/contract/codegenerator/infrastructure/parser/TemplateParserTest.java
@@ -1,30 +1,71 @@
 package com.thanos.contract.codegenerator.infrastructure.parser;
 
+import org.junit.After;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TemplateParserTest {
 
+    private String targetFile = "target/generated-test-sources/DemoTest.java";
+
+    @After
+    public void tearDown() throws Exception {
+        Files.deleteIfExists(Paths.get(targetFile));
+    }
+
     @Test
-    public void parseJunitTemplate() throws IOException {
+    public void parse_to_string() throws IOException {
 
         Map<String, Object> content = new HashMap<>();
+        content.put("className", "Demo");
+        content.put("testMethod", "should_do_sth_when_condition_fulfills");
         content.put("host", "127.0.0.1");
         content.put("port", "12345");
         content.put("plainTextValidation", Arrays.asList("assertTrue(true);", "assertFalse(false);"));
 
         final TemplateParser templateParser = new TemplateParser();
-        final String result = templateParser.parseJunitTemplate(content);
+        final String result = templateParser.parseJunitTemplateToString(content);
 
+        assertTrue(result.contains("DemoTest"));
+        assertTrue(result.contains("should_do_sth_when_condition_fulfills()"));
         assertTrue(result.contains("127.0.0.1"));
         assertTrue(result.contains("12345"));
         assertTrue(result.contains("assertTrue(true);"));
         assertTrue(result.contains("assertFalse(false);"));
+        assertFalse(result.contains("{{}}"));
+    }
+
+    @Test
+    public void parse_to_file() throws IOException {
+
+
+        Map<String, Object> content = new HashMap<>();
+        content.put("className", "Demo");
+        content.put("testMethod", "should_do_sth_when_condition_fulfills");
+        content.put("host", "127.0.0.1");
+        content.put("port", "12345");
+        content.put("plainTextValidation", Arrays.asList("assertTrue(true);", "assertFalse(false);"));
+
+        final TemplateParser templateParser = new TemplateParser();
+        templateParser.parseJunitTemplateToFile(content, targetFile);
+
+        final String result = new String(Files.readAllBytes(Paths.get(targetFile)));
+        assertTrue(result.contains("DemoTest"));
+        assertTrue(result.contains("should_do_sth_when_condition_fulfills()"));
+        assertTrue(result.contains("127.0.0.1"));
+        assertTrue(result.contains("12345"));
+        assertTrue(result.contains("assertTrue(true);"));
+        assertTrue(result.contains("assertFalse(false);"));
+        assertFalse(result.contains("{{}}"));
+
     }
 }
